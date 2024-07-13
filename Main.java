d14a0a7d5fa09ae6139d99dc5769ad62d28d8a7e
kirya <@IlyaGlaz>
2024-07-13 17:56:05 +0300

    cleanig a lot of oshibki

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /**
+        Необходимо посчитать зарплату рабочего за месяц. На консоль вводится, сколько дней в месяце было отработано, дальше вводится вещественное число — зарплата за день. Нужно вывести зарплату рабочего за весь месяц. Например, если в месяце было отработано 10 дней, а зарплата в день была 5500.25 рублей, то программа должна вывести в результате — 55002.5
+         */
+        System.out.println("Введите количество отработаных дней");
                 Scanner scanner = new Scanner(System.in);
              int a = scanner.nextInt();
-             //дневная зп
-        int b = scanner.nextInt();
         //колво отработаных дней
-int result;
-result = a * b;
-                System.out.println(result);
+        System.out.println("Введите дневной доход");
+        int b = scanner.nextInt();
+        //дневная зп
+int result= a * b;
+                System.out.println("Ежемесячная зарпалата:" + result);
     }
 }
